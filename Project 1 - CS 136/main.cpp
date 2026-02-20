d2d7715d1fdd005ab6a9eb42721bb32e06c7a3c1
TaylorSpencer22 <taylor.spencer2@gmail.com>
2026-02-20 11:01:33 -0300

    Initial Commit

---
+++ Project 1 - CS 136/main.cpp
@@ -0,0 +1,28 @@
+//Write a function that accept a linked list of integers and reverses the
+//order of the linked list nodes. The function should have one parameter
+//which is a pointer to the head of the list. After the function is called, this
+//pointer will point to the head of a linked list that has the same nodes as
+//the original list, but in the reverse of the order they had in the original list.
+//Your function should not create or destroy any nodes. It should only rearrange nodes.
+//Write a test program to test your function.
+//..............................................................................
+//What you need to submit is:
+//
+//1- Pseudocode of your function. Can be added as a comment in your source code file.
+//1- Your program source code files (e.g., .cpp extension file).
+//2- Screenshots of your test program output (.png or jpeg file)
+//3- (optionally) you can include the executable file (e.g., .exe file)//
+//  main.cpp
+//  Project 1 - CS 136
+//
+//  Created by Taylor Spencer on 2/19/26.
+//
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
+
+//Pseudocode
